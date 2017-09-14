d13f5bb79f21b1a0c4e5a8c85739b36794bc5981
albertusangga <albertusangga.98@gmail.com>
2017-09-14 11:08:49 +0700

    Add react-countdown-now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+// Documentation : https://www.npmjs.com/package/react-countdown-now
+import Countdown from 'react-countdown-now';
 import logo from './logo.svg';
 import { LandingPageElement } from './style';
+
+const YEAR = 2017;
+const MONTH = 11;
+const DAY = 6;
 
 class App extends Component {
   render() {
     return (
-      <LandingPageElement />
+      <LandingPageElement>
+        <Countdown date={new Date(YEAR, MONTH, DAY)} />
+      </LandingPageElement>
     );
   }
 }
