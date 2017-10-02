66fb582cb8e40a2f363c9f80bd9a7b41824fd0e1
Irene <zirenely@gmail.com>
2017-10-02 22:08:52 +0700

    add logo dewa web

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Countdown from './Countdown';
 import squares from './assets/squares.png';
 import logoRistek from './assets/risteklogo.png';
 import logoTraveloka from './assets/traveloka.png';
+import logoDewaweb from './assets/logo-dewa-web.jpg';
 import footerPattern from './assets/flags.png';
 import { LandingPageElement } from './style';
 
@@ -10,30 +11,33 @@ class App extends Component {
   render() {
     return (
       <LandingPageElement>
-      	<div className="overlay" />
-	      <div className="container">
-	      	<img src={squares} className="squares" alt="square logo" />
-	        <div className="soon">
-	        	COMI NG
-	        </div>
-	        <div className="soon">
-	        	SOON
-	        </div>
-	        <div className="divider" />
-	        <div className="ready">
-	        	BE READY IN
-	        </div>
-	        <Countdown />
-	        <div className="divider" />
-	        <div className="logoContainer">
-	        	<div className="pekanRistek">
-	        		Pekan Ristek
-	        	</div>
-	        	<img src={logoRistek} className="logoRistek" alt="logo ristek" />
-	        </div>
-        	<img src={logoTraveloka} className="logoTraveloka" alt="logo traveloka" />
-        	<img src={footerPattern} className="footerPattern" alt="pattern on footer" />
-	       </div>
+        <div className="overlay" />
+        <div className="container">
+          <img src={squares} className="squares" alt="square logo" />
+          <div className="soon">
+            COMI NG
+          </div>
+          <div className="soon">
+            SOON
+          </div>
+          <div className="divider" />
+          <div className="ready">
+            BE READY IN
+          </div>
+          <Countdown />
+          <div className="divider" />
+          <div className="logoContainer">
+            <div className="pekanRistek">
+              Pekan Ristek
+            </div>
+            <img src={logoRistek} className="logoRistek" alt="logo ristek" />
+          </div>
+          <div className="logoSponsorContainer">
+            <img src={logoTraveloka} className="logoTraveloka" alt="logo traveloka" />
+            <img src={logoDewaweb} className="logoDewaweb" alt="logo dewa web" />
+          </div>
+          <img src={footerPattern} className="footerPattern" alt="pattern on footer" />
+         </div>
       </LandingPageElement>
     );
   }
--- src/style.js
+++ src/style.js
@@ -2,147 +2,159 @@ import styled from 'styled-components';
 
 export const LandingPageElement = styled.div`
 
-	@keyframes fadeOpacity {
-		0% { opacity: 0 }
-		50% { opacity: 1 }
-		100% { opacity: 0}
-	}
+  @keyframes fadeOpacity {
+    0% { opacity: 0 }
+    50% { opacity: 1 }
+    100% { opacity: 0}
+  }
 
-	@keyframes fadeShadow {
-		0% { text-shadow: none; }
-		20% { text-shadow: 3px 3px #aaaaaa; }
-		80% { text-shadow: 3px 3px #aaaaaa; }
-		100% { text-shadow: none; }	
-	}
+  @keyframes fadeShadow {
+    0% { text-shadow: none; }
+    20% { text-shadow: 3px 3px #aaaaaa; }
+    80% { text-shadow: 3px 3px #aaaaaa; }
+    100% { text-shadow: none; } 
+  }
 
-	max-width: 100%;
-	max-height: 100vh;
-	overflow-y: hidden;
-	overflow-x: hidden;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: space-between;
-	color: #FAFAFA;
-	background: linear-gradient(-20deg, #616161 0%, #9bc5c3 100%);
+  max-width: 100%;
+  max-height: 100vh;
+  overflow-y: hidden;
+  overflow-x: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  color: #FAFAFA;
+  background: linear-gradient(-20deg, #616161 0%, #9bc5c3 100%);
 
-	.overlay {
-		width: 100%;
-		height: 100vh;
-		background-image: linear-gradient(120deg, #f6d365 0%, #fda085 100%);
-		position: absolute;
-		z-index: 0;
-		animation: fadeOpacity 10s infinite;
-	}
+  .overlay {
+    width: 100%;
+    height: 100vh;
+    background-image: linear-gradient(120deg, #f6d365 0%, #fda085 100%);
+    position: absolute;
+    z-index: 0;
+    animation: fadeOpacity 10s infinite;
+  }
 
-	.container {
-		width: 100%;
-		height: 100vh;
-		margin: 1rem 0 0 0;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: space-between;
-		z-index: 1;
-	}
+  .container {
+    width: 100%;
+    height: 100vh;
+    margin: 1rem 0 0 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    z-index: 1;
+  }
 
-	.squares {
-		margin: 0 0 1rem 0;
-	}
+  .squares {
+    margin: 0 0 1rem 0;
+  }
 
-	.soon {
-		font-family: Anurati-Regular;
-		font-size: 7rem;
-		line-height: 6rem;
-		letter-spacing: -0.7rem;
-		text-align: center;
-		animation: fadeShadow 10s infinite;
-	}
+  .soon {
+    font-family: Anurati-Regular;
+    font-size: 7rem;
+    line-height: 6rem;
+    letter-spacing: -0.7rem;
+    text-align: center;
+    animation: fadeShadow 10s infinite;
+  }
 
-	.ready {
-		font-family:'Jaapokki-Regular';
-		font-size: 1.5rem;
-		letter-spacing: 1.5rem;
-	}
+  .ready {
+    font-family:'Jaapokki-Regular';
+    font-size: 1.5rem;
+    letter-spacing: 1.5rem;
+  }
 
-	.divider {
-		width: 30rem;
-		height: 0.05rem;
-		background: #FAFAFA;
-		margin: 0.25rem 0;
-	}
+  .divider {
+    width: 30rem;
+    height: 0.05rem;
+    background: #FAFAFA;
+    margin: 0.25rem 0;
+  }
 
-	.logoContainer {
-		width: 30rem;
-		display: flex;
-		flex-direction: row;
-		justify-content: space-around;
-		align-items: center;
+  .logoContainer {
+    width: 30rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
 
-		.pekanRistek {
-			font-family: 'Vermin Vibes 4 Helium';
-			font-size: 2rem;
-		}
+    .pekanRistek {
+      font-family: 'Vermin Vibes 4 Helium';
+      font-size: 2rem;
+    }
 
-		.logoRistek {
-			height: 5rem;
+    .logoRistek {
+      height: 5rem;
 
-		}
+    }
 
-	}
+  }
 
-	.logoTraveloka {
-		height: 2.5rem;
-		margin: 0.5rem;
-	}
+  .logoSponsorContainer {
+    display: flex;
+    height: 3.5rem;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
 
-	.footerPattern {
-		width: 100%;
-		objectFit: cover;
-	}
+  .logoTraveloka {
+    height: 2.5rem;
+    margin: 0.2rem;
+  }
 
-	@media screen and (max-width: 40em) {
+  .logoDewaweb {
+    height: 2.5rem;
+  }
 
-		.container {
-			margin: 0.5rem;
-			justify-content: space-around;
-		}
+  .footerPattern {
+    width: 100%;
+    objectFit: cover;
+  }
 
-		.squares {
-			margin: 0 0 0.5rem 0;
-		}
+  @media screen and (max-width: 40em) {
 
-		.soon {
-			font-size: 4rem;
-			letter-spacing: -0.3rem;
-			line-height: 3rem;
-		}
+    .container {
+      margin: 0.5rem;
+      justify-content: space-around;
+    }
 
-		.ready {
-			font-size: 1rem;
-			letter-spacing: 0.8rem;
-		}
+    .squares {
+      margin: 0 0 0.5rem 0;
+    }
 
-		.divider {
-			max-width: 15rem;
-			margin: 0;
-		}
+    .soon {
+      font-size: 4rem;
+      letter-spacing: -0.3rem;
+      line-height: 3rem;
+    }
 
-		.logoContainer {
-			width: 100%;
-			padding: 0 2rem;
+    .ready {
+      font-size: 1rem;
+      letter-spacing: 0.8rem;
+    }
 
-			.pekanRistek{
-				font-size: 1rem;
-			}
+    .divider {
+      max-width: 15rem;
+      margin: 0;
+    }
 
-			.logoRistek {
-				height: 3rem;
-			}
-		}
+    .logoContainer {
+      width: 100%;
+      padding: 0 2rem;
 
-		.footerPattern {
-			display: none;
-		}
-	}
+      .pekanRistek{
+        font-size: 1rem;
+      }
+
+      .logoRistek {
+        height: 3rem;
+      }
+    }
+
+    .footerPattern {
+      display: none;
+    }
+  }
 `;
